a9bbdfa33ad643b77857da9f8ebefe6072a893b1
KwangryeolPark <pkr7098@gmail.com>
2025-01-06 11:06:16 +0900

    git_update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Python package for sending email',
     author='Kwangryeol Park',
     author_email='pkr7098@gmail.com',
-    url='',
+    url='https://github.com/KwangryeolPark/python-email-sender',
     packages=find_packages(),  # 이 부분을 수정하여 모든 패키지를 자동으로 찾도록 함
     package_data={},
 )
